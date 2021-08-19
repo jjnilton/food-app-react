5d667902e2c88f39630db983933b20d3cae53f26
JJ <jjnilton@users.noreply.github.com>
2021-08-19 14:29:23 -0300

    style adjustments

--- src/components/Checkout/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm.js
@@ -138,201 +138,210 @@ const CheckoutForm = (props) => {
     creditCardInfoValid;
 
   const handleFill = () => {
-    firstNameChange({ target: { value: "First"}})
-    lastNameChange({ target: { value: "Last"}})
-    documentChange({ target: { value: "1234567890"}})
-    addressChange({ target: { value: "Nowhere Street, 73, BA"}})
-    creditCardChange({ target: { value: "1234567890123456"}})
-    creditCardSecretChange({ target: { value: "123"}})
-    creditCardExpirationChange({ target: { value: "21/12"}})
-    creditCardOwnerChange({ target: { value: "First F. L. Last"}})
-  }
+    firstNameChange({ target: { value: "First" } });
+    lastNameChange({ target: { value: "Last" } });
+    documentChange({ target: { value: "1234567890" } });
+    addressChange({ target: { value: "Nowhere Street, 73, BA" } });
+    creditCardChange({ target: { value: "1234567890123456" } });
+    creditCardSecretChange({ target: { value: "123" } });
+    creditCardExpirationChange({ target: { value: "21/12" } });
+    creditCardOwnerChange({ target: { value: "First F. L. Last" } });
+  };
 
   return (
     <>
-    <button onClick={handleFill} style={{position: "absolute", transform: "translate(0px, -20px)"}}>fill form</button>
-    <form
-      id="checkout-form"
-      className={styles["checkout-form"]}
-      method="post"
-      onSubmit={handleSubmit}
-    >
-      <fieldset className={styles["client-info"]}>
-        <legend>User information</legend>
-        <div className={styles["first-and-second-name"]}>
-          <div>
-            <label htmlFor="first-name">First Name</label>
-            <input
-              className={firstNameError ? styles.invalid : undefined}
-              ref={firstNameRef}
-              name="first-name"
-              type="text"
-              value={firstNameValue}
-              onChange={firstNameChange}
-              onBlur={firstNameBlur}
-            />
-            {firstNameError && (
-              <div className={styles["invalid-message"]}>Invalid name.</div>
-            )}
-          </div>
-          <div>
-            <label htmlFor="last-name">Last Name</label>
-            <input
-              className={lastNameError ? styles.invalid : undefined }
-              ref={lastNameRef}
-              name="last-name"
-              type="text"
-              value={lastNameValue}
-              onChange={lastNameChange}
-              onBlur={lastNameBlur}
-            />
-            {lastNameError && (
-              <div className={styles["invalid-message"]}>
-                Invalid last name.
-              </div>
-            )}
-          </div>
-        </div>
-        <label htmlFor="document">Document</label>
-        <input
-          className={documentError ? styles.invalid : undefined}
-          name="document"
-          type="text"
-          value={documentValue}
-          onChange={documentChange}
-          onBlur={documentBlur}
-        />
-        {documentError && (
-          <div className={styles["invalid-message"]}>Invalid document.</div>
-        )}
-      </fieldset>
-      <fieldset className={styles["address-info"]}>
-        <legend>Delivery info</legend>
-        <label htmlFor="address">Address</label>
-        <input
-          className={addressError ? styles.invalid : undefined}
-          name="address"
-          type="text"
-          value={addressValue}
-          onChange={handleAddressChange}
-          onBlur={addressBlur}
-        />
-        {addressError && (
-          <div className={styles["invalid-message"]}>Invalid address.</div>
-        )}
-      </fieldset>
-      <fieldset className={styles["payment-info"]}>
-        <legend>Payment information</legend>
-        <div className={styles["payment-options"]}>
-          <input
-            type="radio"
-            name="payment-method"
-            id="credit-card"
-            value="credit-card"
-            onChange={handleRadioChange}
-            checked={checkedRadio === "credit-card"}
-          />
-          <label htmlFor="credit-card">Credit card</label>
-          <input
-            type="radio"
-            name="payment-method"
-            id="libera-pay"
-            value="libera-pay"
-            onChange={handleRadioChange}
-            checked={checkedRadio === "libera-pay"}
-          />
-          <label htmlFor="libera-pay">LiberaPay</label>
-        </div>
-        {checkedRadio === "credit-card" && (
-          <div className={styles["credit-card-info"]}>
-            <label htmlFor="credit-card-number">Number</label>
-            <input
-              className={creditCardError ? styles.invalid : undefined}
-              name="credit-card-number"
-              type="text"
-              onChange={creditCardChange}
-              onBlur={creditCardBlur}
-              value={creditCardValue}
-            />
-            {creditCardError && (
-              <div className={styles["invalid-message"]}>
-                Invalid credit card number.
-              </div>
-            )}
-            <div className={styles["secret-and-expiration"]}>
-              <div>
-                <label htmlFor="credit-card-secret">Secret</label>
-                <input
-                  className={creditCardSecretError ? styles.invalid : undefined}
-                  name="credit-card-secret"
-                  type="text"
-                  maxLength="3"
-                  onChange={creditCardSecretChange}
-                  onBlur={creditCardSecretBlur}
-                  value={creditCardSecretValue}
-                />
-                {creditCardSecretError && (
-                  <div className={styles["invalid-message"]}>
-                    Invalid card secret.
-                  </div>
-                )}
-              </div>
-              <div>
-                <label htmlFor="expiration-date">Expiration Date</label>
-                <input
-                  className={creditCardExpirationError ? styles.invalid : undefined}
-                  name="expiration-date"
-                  type="text"
-                  placeholder="YY/mm"
-                  maxLength="5"
-                  onChange={creditCardExpirationChange}
-                  onBlur={creditCardExpirationBlur}
-                  value={creditCardExpirationValue}
-                />
-                {creditCardSecretError && (
-                  <div className={styles["invalid-message"]}>
-                    Invalid expiration.
-                  </div>
-                )}
-              </div>
+      <button
+        onClick={handleFill}
+        style={{ position: "absolute", transform: "translate(0px, -20px)" }}
+      >
+        fill form
+      </button>
+      <form
+        id="checkout-form"
+        className={styles["checkout-form"]}
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <fieldset className={styles["client-info"]}>
+          <legend>User information</legend>
+          <div className={styles["first-and-second-name"]}>
+            <div>
+              <label htmlFor="first-name">First Name</label>
+              <input
+                className={firstNameError ? styles.invalid : undefined}
+                ref={firstNameRef}
+                name="first-name"
+                type="text"
+                value={firstNameValue}
+                onChange={firstNameChange}
+                onBlur={firstNameBlur}
+              />
+              {firstNameError && (
+                <div className={styles["invalid-message"]}>Invalid name.</div>
+              )}
             </div>
-            <div className={styles["credit-card-owner-container"]}>
-              <label htmlFor="card-owner">Name</label>
-              <div>
-                <input
-                  name="same-name"
-                  type="checkbox"
-                  onChange={handleCheckbox}
-                />
-                <label className={styles["same-name"]} htmlFor="same-name">
-                  Copy from user info
-                </label>
-              </div>
+            <div>
+              <label htmlFor="last-name">Last Name</label>
+              <input
+                className={lastNameError ? styles.invalid : undefined}
+                ref={lastNameRef}
+                name="last-name"
+                type="text"
+                value={lastNameValue}
+                onChange={lastNameChange}
+                onBlur={lastNameBlur}
+              />
+              {lastNameError && (
+                <div className={styles["invalid-message"]}>
+                  Invalid last name.
+                </div>
+              )}
             </div>
+          </div>
+          <label htmlFor="document">Document</label>
+          <input
+            className={documentError ? styles.invalid : undefined}
+            name="document"
+            type="text"
+            value={documentValue}
+            onChange={documentChange}
+            onBlur={documentBlur}
+          />
+          {documentError && (
+            <div className={styles["invalid-message"]}>Invalid document.</div>
+          )}
+        </fieldset>
+        <fieldset className={styles["address-info"]}>
+          <legend>Delivery info</legend>
+          <label htmlFor="address">Address</label>
+          <input
+            className={addressError ? styles.invalid : undefined}
+            name="address"
+            type="text"
+            value={addressValue}
+            onChange={handleAddressChange}
+            onBlur={addressBlur}
+          />
+          {addressError && (
+            <div className={styles["invalid-message"]}>Invalid address.</div>
+          )}
+        </fieldset>
+        <fieldset className={styles["payment-info"]}>
+          <legend>Payment information</legend>
+          <div className={styles["payment-options"]}>
             <input
-              className={creditCardOwnerError ? styles.invalid : undefined}
-              name="card-owner"
-              type="text"
-              value={creditCardOwnerValue}
-              onChange={creditCardOwnerChange}
-              onBlur={creditCardOwnerBlur}
+              type="radio"
+              name="payment-method"
+              id="credit-card"
+              value="credit-card"
+              onChange={handleRadioChange}
+              checked={checkedRadio === "credit-card"}
             />
-            {creditCardOwnerError && (
-              <div className={styles["invalid-message"]}>
-                Invalid credit card name.
+            <label htmlFor="credit-card">Credit card</label>
+            <input
+              type="radio"
+              name="payment-method"
+              id="libera-pay"
+              value="libera-pay"
+              onChange={handleRadioChange}
+              checked={checkedRadio === "libera-pay"}
+            />
+            <label htmlFor="libera-pay">LiberaPay</label>
+          </div>
+          {checkedRadio === "credit-card" && (
+            <div className={styles["credit-card-info"]}>
+              <label htmlFor="credit-card-number">Number</label>
+              <input
+                className={creditCardError ? styles.invalid : undefined}
+                name="credit-card-number"
+                type="text"
+                onChange={creditCardChange}
+                onBlur={creditCardBlur}
+                value={creditCardValue}
+              />
+              {creditCardError && (
+                <div className={styles["invalid-message"]}>
+                  Invalid credit card number.
+                </div>
+              )}
+              <div className={styles["secret-and-expiration"]}>
+                <div>
+                  <label htmlFor="credit-card-secret">Secret</label>
+                  <input
+                    className={
+                      creditCardSecretError ? styles.invalid : undefined
+                    }
+                    name="credit-card-secret"
+                    type="text"
+                    maxLength="3"
+                    onChange={creditCardSecretChange}
+                    onBlur={creditCardSecretBlur}
+                    value={creditCardSecretValue}
+                  />
+                  {creditCardSecretError && (
+                    <div className={styles["invalid-message"]}>
+                      Invalid card secret.
+                    </div>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="expiration-date">Expiration Date</label>
+                  <input
+                    className={
+                      creditCardExpirationError ? styles.invalid : undefined
+                    }
+                    name="expiration-date"
+                    type="text"
+                    placeholder="YY/mm"
+                    maxLength="5"
+                    onChange={creditCardExpirationChange}
+                    onBlur={creditCardExpirationBlur}
+                    value={creditCardExpirationValue}
+                  />
+                  {creditCardSecretError && (
+                    <div className={styles["invalid-message"]}>
+                      Invalid expiration.
+                    </div>
+                  )}
+                </div>
               </div>
-            )}
-          </div>
-        )}
-        {checkedRadio === "libera-pay" && (
-          <div>
-            <p>
-              You will be redirected to the page after completing the order.
-            </p>
-          </div>
-        )}
-      </fieldset>
-    </form>
+              <div className={styles["credit-card-owner-container"]}>
+                <label htmlFor="card-owner">Name</label>
+                <div>
+                  <input
+                    name="same-name"
+                    type="checkbox"
+                    onChange={handleCheckbox}
+                  />
+                  <label className={styles["same-name"]} htmlFor="same-name">
+                    Copy from user info
+                  </label>
+                </div>
+              </div>
+              <input
+                className={creditCardOwnerError ? styles.invalid : undefined}
+                name="card-owner"
+                type="text"
+                value={creditCardOwnerValue}
+                onChange={creditCardOwnerChange}
+                onBlur={creditCardOwnerBlur}
+              />
+              {creditCardOwnerError && (
+                <div className={styles["invalid-message"]}>
+                  Invalid credit card name.
+                </div>
+              )}
+            </div>
+          )}
+          {checkedRadio === "libera-pay" && (
+            <div>
+              <p>
+                You will be redirected to the page after completing the order.
+              </p>
+            </div>
+          )}
+        </fieldset>
+      </form>
     </>
   );
 };
